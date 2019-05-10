1ff83ed5e4d686edec0c580dc787d3a043676daa
Andres Ladino <aladinoster@gmail.com>
2019-05-10 07:32:39 +0200

    Adding ex-4

---
+++ ch_1/4_largest_prime.cpp
@@ -0,0 +1,15 @@
+/*
+Write a program that computes and prints the largest prime number that is smaller than a number provided by the user, which must be a positive integer.
+*/
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n, max;
+
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    return 0;
+}
