f9d2bf4ef6ddc39d85155a725dd7e685117d4b92
Andres Ladino <aladinoster@gmail.com>
2019-05-10 07:28:28 +0200

    Solving the lcm enigma

--- ch_1/3_lcm_twonumbers_sol.cpp
+++ ch_1/3_lcm_twonumbers_sol.cpp
@@ -4,27 +4,31 @@ Write a program that will, given two or more positive integers, calculate and pr
 #include <iostream>
 // using namespace std;
 
+unsigned int gcd(unsigned int a, unsigned int b)
+{
+    while (b != 0)
+    {
+        unsigned int r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+int lcm(int const a, int const b)
+{
+    int h = gcd(a, b);
+    return h ? (a * (b / h)) : 0;
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
-    unsigned int number_1, number_2, lcm;
+    unsigned int number_1, number_2;
     std::cout << "Please enter number 1: " << std::endl;
     std::cin >> number_1;
     std::cout << "Please enter number 2: " << std::endl;
     std::cin >> number_2;
 
-    lcm = number_1 > number_2 ? number_1 : number_2;
-
-    do
-    {
-        if (lcm % number_1 == 0 && lcm % number_2 == 0)
-        {
-            std::cout << "The least common multiple is: " << lcm << std::endl;
-            break;
-        }
-        else
-            lcm++;
-    } while (true);
-
+    std::cout << "The least common multiple is: " << lcm(number_1, number_2) << std::endl;
     return 0;
 }
