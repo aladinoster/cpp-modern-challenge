18941b484d5b083ca7d6b9b8b4a41fb391c46364
Andres Ladino <aladinoster@gmail.com>
2019-05-16 21:06:55 +0200

    Fixing merge

--- ch_1/4_largest_prime.cpp
+++ ch_1/4_largest_prime.cpp
@@ -41,7 +41,7 @@ int main(int argc, char const *argv[])
             std::cout << "Largest prime before " << limit << " is: " << i << std::endl;
             return 0;
         }
-    }
+    } 
 
     return 0;
 }
